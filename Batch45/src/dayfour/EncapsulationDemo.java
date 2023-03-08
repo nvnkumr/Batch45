39ec573fe6684692ae63f50c85d634d9c5873712
hp <hp@DESKTOP-6RJKTI4>
2023-03-08 14:06:21 +0530

    Second commit

---
+++ Batch45/src/dayfour/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package dayfour;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		
+		// object creation
+				OopsConcepts obj = new OopsConcepts();
+				obj.setSerialNum(101);
+				obj.setName("naveen");
+				obj.setAge(20);
+				System.out.println(obj);
+
+	}
+
+}
